e9354141ada3acd40c197ba70dd744931f5bd244
Michael Chou <snoopy02m@gmail.com>
2023-03-02 17:33:16 +0800

    perf: add combine function for texthandler

--- text/text_handler.py
+++ text/text_handler.py
@@ -17,21 +17,29 @@ class TextHandler:
         'sans-serif'
     })
 
-    _EMAIL_VERIFICATION_TEMPLATE = None
+    _TASK_REGESTER = 'register'
+    _TASK_RESET_PASSWORD = 'reset_password'
+
+    _TEMP_MAP = {
+        _TASK_REGESTER: '',
+        _TASK_RESET_PASSWORD: '',
+    }
     """ EMAIL """
 
     @classmethod
-    def _get_email_verification_template(cls):
-        if cls._EMAIL_VERIFICATION_TEMPLATE is None:
-            with open('common/text/email_verification.html', 'r') as fr:
-                cls._EMAIL_VERIFICATION_TEMPLATE = fr.read()
-        return cls._EMAIL_VERIFICATION_TEMPLATE
+    def _read(cls, task, path):
+        if not cls._TEMP_MAP[task]:
+            with open(path, 'r') as fr:
+                cls._TEMP_MAP[task] = fr.read()
+        return cls._TEMP_MAP[task]
 
     @classmethod
     def get_verification_email(cls, otp):
-        template = cls._get_email_verification_template()
+        template = cls._read(task=cls._TASK_REGESTER,
+                             path='common/text/email_verification.html')
         expiration = Toolkit.convert_seconds(
-            seconds=Const.VerificationSystem.EMAIL_OTP_EXPIRATION, unit='hours')
+            seconds=Const.VerificationSystem.EMAIL_OTP_EXPIRATION,
+            unit='minutes')
         font_families = cls._FONT_FAMILIES
         content = Template(template).substitute(
             app_name=cls._APP_NAME,
@@ -41,4 +49,18 @@ class TextHandler:
             current_year=date.today().year,
         )
         title = f'Welcome to {cls._APP_NAME}'
+        return title, content
+
+    @classmethod
+    def get_reset_password(cls, otp):
+        template = cls._read(task=cls._TASK_RESET_PASSWORD,
+                             path='common/text/reset_password.html')
+        font_families = cls._FONT_FAMILIES
+        content = Template(template).substitute(
+            app_name=cls._APP_NAME,
+            font_families=font_families,
+            code=otp,
+            current_year=date.today().year,
+        )
+        title = f'Reset Password in {cls._APP_NAME}'
         return title, content
--- utils/email_handler.py
+++ utils/email_handler.py
@@ -11,6 +11,11 @@ class EmailHandler:
     _SENDER = (config['APP_NAME'], config['MAIL_USERNAME'])
     _APP_NAME = config['APP_NAME']
 
+    _MAP = {
+        TextHandler._TASK_REGESTER: TextHandler.get_verification_email,
+        TextHandler._TASK_RESET_PASSWORD: TextHandler.get_reset_password,
+    }
+
     @classmethod
     def _send(cls, email, title, content):
         if isinstance(email, str):
@@ -25,9 +30,9 @@ class EmailHandler:
         print(f'[{title}] to {email}')
 
     @classmethod
-    def send_verification(cls, email):
+    def send_verification(cls, email, task):
         otp = KeyGenerator.get_random_code(number=5, has_digit=True)
-        title, content = TextHandler.get_verification_email(otp=otp)
+        title, content = cls._MAP[task](otp=otp)
         DataCache.del_verify_email_otp(email=email)
         DataCache.set_verify_email_otp(email=email, otp=otp)
         with app.app_context():
@@ -37,26 +42,30 @@ class EmailHandler:
     def verify_email(cls, email, otp):
         attempts = DataCache.get_verify_email_attempt(email=email)
         if not attempts:
-            raise ValidationError(error_code=ErrorCode.EMAIL_NOT_VERIFIED, message='email is incorrect')
+            raise ValidationError(error_code=ErrorCode.EMAIL_NOT_VERIFIED,
+                                  message='email is incorrect')
         valid_otp = DataCache.get_verify_email_otp(email=email)
         if not valid_otp:
             message = 'OTP is expired, please start over again'
-            raise ValidationError(error_code=ErrorCode.OTP_IS_EXPIRED, message=message)
+            raise ValidationError(error_code=ErrorCode.OTP_IS_EXPIRED,
+                                  message=message)
         if otp != valid_otp:
             message = f'OTP: {otp} is incorrect'
-            raise ValidationError(error_code=ErrorCode.INVALID_OTP, message=message)
-        DataCache.del_verify_email_otp(email=email)
+            raise ValidationError(error_code=ErrorCode.INVALID_OTP,
+                                  message=message)
 
     @classmethod
     def verify_verified_email(cls, email, otp):
         attempts = DataCache.get_verify_email_attempt(email=email)
         if not attempts:
-            raise ValidationError(error_code=ErrorCode.EMAIL_NOT_VERIFIED, message='email is incorrect')
+            raise ValidationError(error_code=ErrorCode.EMAIL_NOT_VERIFIED,
+                                  message='email is incorrect')
         valid_otp = DataCache.get_verified_email(email=email)
         if not valid_otp:
             message = 'OTP is expired, please start over again'
-            raise ValidationError(error_code=ErrorCode.OTP_IS_EXPIRED, message=message)
+            raise ValidationError(error_code=ErrorCode.OTP_IS_EXPIRED,
+                                  message=message)
         if otp != valid_otp:
             message = f'OTP: {otp} is incorrect'
-            raise ValidationError(error_code=ErrorCode.INVALID_OTP, message=message)
-        # DataCache.del_verified_email(email=email)
+            raise ValidationError(error_code=ErrorCode.INVALID_OTP,
+                                  message=message)
